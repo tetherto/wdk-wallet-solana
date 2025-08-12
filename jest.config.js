9ad0f7ea7692cf8d3cb4ce47dcb4f63f51859907
Kulwinder singh <kulwinder.singh@tether.io>
2025-08-12 15:50:49 +0530

    chore: remove integration test

--- jest.config.js
+++
@@ -1,6 +0,0 @@
-export default {
-  globalSetup: process.env.SKIP_SETUP ? undefined : './tests/integration/jest.setup.js',
-  globalTeardown: process.env.SKIP_TEARDOWN ? undefined : './tests/integration/jest.teardown.js',
-  testEnvironment: 'node',
-  testTimeout: 30_000
-}
